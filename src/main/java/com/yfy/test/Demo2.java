0fc8d315abd36ddc0ac0207ff25deedde46dffae
yangfengyu <yangfengyu@anqu.com>
2020-09-28 10:54:19 +0800

    update demo2

--- src/main/java/com/yfy/test/Demo2.java
+++ src/main/java/com/yfy/test/Demo2.java
@@ -2,6 +2,6 @@ package com.yfy.test;
 
 public class Demo2 {
     public static void main(String[] args) {
-        System.out.println("demo2");
+        System.out.println("demo2 update");
     }
 }
