3930d378c0942dd9879f4398f48a2b01cda47548
yfy <study@163.com>
2020-09-28 14:47:43 +0800

    add log 1

--- src/main/java/com/yfy/test/Demo2.java
+++ src/main/java/com/yfy/test/Demo2.java
@@ -3,5 +3,7 @@ package com.yfy.test;
 public class Demo2 {
     public static void main(String[] args) {
         System.out.println("demo2 update");
+
+        System.out.println("log 1");
     }
 }
